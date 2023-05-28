ea86bd4034e88318a961bb6b11fd07488eaa2c18
Sven H <sven.hjol@protonmail.com>
2023-05-28 23:47:07 +0000

    Update signatures to use GuiGraphics

--- src/main/java/svenhjol/charm_core/base/CharmContainerScreen.java
+++ src/main/java/svenhjol/charm_core/base/CharmContainerScreen.java
@@ -2,6 +2,7 @@ package svenhjol.charm_core.base;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
@@ -14,33 +15,33 @@ public abstract class CharmContainerScreen<T extends AbstractContainerMenu> exte
 
     public CharmContainerScreen(T menu, Inventory inv, Component titleIn, ResourceLocation texture) {
         super(menu, inv, titleIn);
-        this.passEvents = true;
         this.texture = texture;
     }
 
     @Override
-    public void render(PoseStack matrices, int mouseX, int mouseY, float delta) {
-        renderBackground(matrices);
-        super.render(matrices, mouseX, mouseY, delta);
-        renderTooltip(matrices, mouseX, mouseY);
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
+        renderBackground(guiGraphics);
+        super.render(guiGraphics, mouseX, mouseY, delta);
+        renderTooltip(guiGraphics, mouseX, mouseY);
     }
 
     @Override
-    protected void renderLabels(PoseStack matrices, int mouseX, int mouseY) {
-        this.font.draw(matrices, this.title.getVisualOrderText(), 8.0F, 6.0F, 4210752);
-        this.font.draw(matrices, this.playerInventoryTitle.getVisualOrderText(), 8.0F, (float) imageHeight - 94, 4210752);
+    protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
+        guiGraphics.drawString(this.font, this.title, 8, 6, 4210752);
+        guiGraphics.drawString(this.font, this.playerInventoryTitle, 8, imageHeight - 94, 4210752);
     }
 
     @Override
-    protected void renderBg(PoseStack matrices, float delta, int mouseX, int mouseY) {
+    protected void renderBg(GuiGraphics guiGraphics, float delta, int mouseX, int mouseY) {
         if (minecraft != null) {
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-            RenderSystem.setShaderTexture(0, texture);
+            // CHECK: might not be required?
+//            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+//            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+//            RenderSystem.setShaderTexture(0, texture);
 
             var x = (width - imageWidth) / 2;
             var y = (height - imageHeight) / 2;
-            this.blit(matrices, x, y, 0, 0, imageWidth, imageHeight);
+            guiGraphics.blit(texture, x, y, 0, 0, imageWidth, imageHeight);
         }
     }
 }
