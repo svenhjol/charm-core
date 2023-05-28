22ff13afdefbe94ce33cb006736be3857ed42c68
Sven H <sven.hjol@protonmail.com>
2023-05-28 23:48:55 +0000

    Fix RenderTooltipCallback definition

--- src/main/java/svenhjol/charm_core/fabric/event/RenderTooltipCallback.java
+++ src/main/java/svenhjol/charm_core/fabric/event/RenderTooltipCallback.java
@@ -1,21 +1,19 @@
 package svenhjol.charm_core.fabric.event;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
-import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.world.item.ItemStack;
 
-import javax.annotation.Nullable;
 import java.util.List;
 
 public interface RenderTooltipCallback {
-    Event<RenderTooltipCallback> EVENT = EventFactory.createArrayBacked(RenderTooltipCallback.class, (listeners) -> (screen, poseStack, stack, lines, x, y) -> {
+    Event<RenderTooltipCallback> EVENT = EventFactory.createArrayBacked(RenderTooltipCallback.class, (listeners) -> (guiGraphics, stack, lines, x, y) -> {
         for (RenderTooltipCallback listener : listeners) {
-            listener.interact(screen, poseStack, stack, lines, x, y);
+            listener.interact(guiGraphics, stack, lines, x, y);
         }
     });
 
-    void interact(Screen screen, PoseStack poseStack, @Nullable ItemStack stack, List<ClientTooltipComponent> lines, int x, int y);
+    void interact(GuiGraphics guiGraphics, ItemStack stack, List<ClientTooltipComponent> lines, int x, int y);
 }
